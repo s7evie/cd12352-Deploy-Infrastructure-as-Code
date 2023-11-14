6dec7ec598144a238ba5463d4f69f28681eae3b4
s7evie <s7evie@gmail.com>
2023-11-14 10:14:44 +0000

    commit

--- networking-and-infrastructure/create.sh
+++ networking-and-infrastructure/create.sh
@@ -2,4 +2,4 @@ aws cloudformation create-stack \
    --stack-name udacity-workflow-lesson \
    --template-body file://workflow-and-helpers.yml \
    --parameters file://workflow-and-helpers-parameters.json \
-   --region us-east-1 
+   --region us-east-1
